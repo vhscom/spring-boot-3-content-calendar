2a7ac124a3abb2f621d4f76de153537a21bb454f
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 17:54:48 -0400

    spike on jdbc template repo crud

--- src/main/java/com/vhsdev/contentcalendar/repository/ContentJdbcTemplateRepository.java
+++ src/main/java/com/vhsdev/contentcalendar/repository/ContentJdbcTemplateRepository.java
@@ -16,7 +16,63 @@ public class ContentJdbcTemplateRepository {
     this.jdbcTemplate = jdbcTemplate;
   }
 
-  // crud methods
-  // ...
+  private static Object mapRowToContent(ResultSet rs, int rowNum) throws SQLException {
+    return new Content(
+        rs.getInt("id"),
+        rs.getString("title"),
+        rs.getString("description"),
+        rs.getString("status"),
+        rs.getString("type"),
+        rs.getTimestamp("date_created").toLocalDateTime(),
+        rs.getTimestamp("date_updated").toLocalDateTime(),
+        rs.getString("url")
+    );
+  }
 
+  public List<Content> getAllContent() {
+    var sql = "SELECT * FROM content";
+    List<Content> contentList = jdbcTemplate.query(sql, ContentJdbcTemplateRepository::mapRowToContent);
+    return contentList;
+  }
+
+  public void createContent(Content content) {
+    var sql = "INSERT INTO content (title, description, status, type, date_created, date_updated, url) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    jdbcTemplate.update(
+        sql,
+        content.title(),
+        content.description(),
+        content.status().name(),
+        content.contentType().name(),
+        content.dateCreated(),
+        content.dateUpdated(),
+        content.url()
+    );
+  }
+
+  public void updateContent(Content content) {
+    var sql = "UPDATE content SET title = ?, description = ?, status = ?, type = ?, date_created = ?, date_updated = ?, url = ? WHERE id = ?";
+    jdbcTemplate.update(
+        sql,
+        content.title(),
+        content.description(),
+        content.status().name(),
+        content.contentType().name(),
+        content.dateCreated(),
+        content.dateUpdated(),
+        content.url(),
+        content.id()
+    );
+  }
+
+  public void deleteContent(Integer id) {
+    var sql = "DELETE FROM content WHERE id = ?";
+    jdbcTemplate.update(sql, id);
+  }
+
+  public Content getContentById(Integer id) {
+    var sql = "SELECT * FROM content WHERE id = ?";
+    // This is using a deprecated method. It is just for demonstration purposes.
+    Content content = (Content) jdbcTemplate.queryForObject(sql, new Object[]{id}, ContentJdbcTemplateRepository::mapRowToContent);
+    return content;
+  }
 }
