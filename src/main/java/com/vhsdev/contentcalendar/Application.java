4aedc46dc5c5e38cab87866489a491ca13567138
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 13:00:31 -0400

    initial commit

---
+++ src/main/java/com/vhsdev/contentcalendar/Application.java
@@ -0,0 +1,19 @@
+package com.vhsdev.contentcalendar;
+
+import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+    // Handy trick for getting a list of all the Beans in the application context
+    // Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    // IntelliJ IDEA has a built-in feature for this, but it's still a handy trick to know.
+  }
+
+}
