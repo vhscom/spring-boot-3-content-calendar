46025b2357089ff231995df73e4230a234d2acd4
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 15:50:00 -0400

    add basic cors support

--- src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
@@ -4,6 +4,7 @@ import com.vhsdev.contentcalendar.model.Content;
 import com.vhsdev.contentcalendar.repository.ContentCollectionRepository;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/content")
+@CrossOrigin
 public class ContentController {
 
   private final ContentCollectionRepository repository;
