f46ce425abda95973624cbe34ec75e3b2a2e2d82
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 14:24:40 -0400

    regisiter and initialize repository

--- src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
@@ -1,6 +1,9 @@
 package com.vhsdev.contentcalendar.controller;
 
+import com.vhsdev.contentcalendar.model.Content;
 import com.vhsdev.contentcalendar.repository.ContentCollectionRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,10 @@ public class ContentController {
 
   public ContentController(ContentCollectionRepository repository) {
     this.repository = repository;
+  }
+
+  @GetMapping("")
+  public List<Content> findAll() {
+    return repository.findAll();
   }
 }
--- src/main/java/com/vhsdev/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/vhsdev/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,10 +1,16 @@
 package com.vhsdev.contentcalendar.repository;
 
 import com.vhsdev.contentcalendar.model.Content;
+import com.vhsdev.contentcalendar.model.Status;
+import com.vhsdev.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ContentCollectionRepository {
 
   private final List<Content> content = new ArrayList<>();
@@ -18,5 +24,20 @@ public class ContentCollectionRepository {
 
   public Optional<Content> findById(Integer id) {
     return content.stream().filter(c -> c.id().equals(id)).findFirst();
+  }
+
+  @PostConstruct
+  private void init() {
+    Content c = new Content(
+        1,
+        "Spring Boot",
+        "Spring Boot is a Java framework",
+        Status.PUBLISHED,
+        Type.ARTICLE,
+        LocalDateTime.now(),
+        null,
+        "https://spring.io/projects/spring-boot"
+    );
+    content.add(c);
   }
 }
