93e6c9bce78fa3cdc2377d6e50b1d057169e6358
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 14:07:05 -0400

    wire content collection repo to controller

--- src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
@@ -1,5 +1,6 @@
 package com.vhsdev.contentcalendar.controller;
 
+import com.vhsdev.contentcalendar.repository.ContentCollectionRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/content")
 public class ContentController {
 
+  private final ContentCollectionRepository repository;
+
+  public ContentController(ContentCollectionRepository repository) {
+    this.repository = repository;
+  }
 }
