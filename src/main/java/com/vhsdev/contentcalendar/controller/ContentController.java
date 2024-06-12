dcd177f2d078b87ae41c350a6cccc61606d446e6
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 14:44:11 -0400

    return 404 when not found by id

--- src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
@@ -3,10 +3,12 @@ package com.vhsdev.contentcalendar.controller;
 import com.vhsdev.contentcalendar.model.Content;
 import com.vhsdev.contentcalendar.repository.ContentCollectionRepository;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/content")
@@ -25,6 +27,8 @@ public class ContentController {
 
   @GetMapping("/{id}")
   public Content findById(@PathVariable Integer id) {
-    return repository.findById(id).orElse(null);
+    return repository.findById(id).orElseThrow(
+        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found")
+    );  
   }
 }
