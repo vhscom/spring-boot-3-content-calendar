fd031d96ba5fbe59dda70823b2496fb8de145a3f
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 13:54:55 -0400

    add controller request mapping

--- src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
@@ -1,8 +1,10 @@
 package com.vhsdev.contentcalendar.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/content")
 public class ContentController {
 
 }
