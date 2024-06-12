2653923c2a910def2555d2e1eba8a07b4ade2214
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 13:06:51 -0400

    get bean info by name

--- src/main/java/com/vhsdev/contentcalendar/Application.java
+++ src/main/java/com/vhsdev/contentcalendar/Application.java
@@ -1,12 +1,18 @@
 package com.vhsdev.contentcalendar;
 
 import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
+
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
@@ -14,6 +20,11 @@ public class Application {
     // Handy trick for getting a list of all the Beans in the application context
     Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     // IntelliJ IDEA has a built-in feature for this, but it's still a handy trick to know.
+
+    // You can also get a specific Bean by name
+    // We cast the Bean to a RestTemplate because we know that's what it is
+    RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+    log.info("RestTemplate Bean: {}", restTemplate);
   }
 
 }
