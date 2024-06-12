54107b96c08edcd998657278da0c2f7560e4d3e8
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 15:54:17 -0400

    begin adding content validation

--- src/main/java/com/vhsdev/contentcalendar/model/Content.java
+++ src/main/java/com/vhsdev/contentcalendar/model/Content.java
@@ -1,9 +1,12 @@
 package com.vhsdev.contentcalendar.model;
 
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public record Content(
     Integer id,
+    // If we did nothing else, this wouldn't work. Use along with @Valid in the controller.
+    @NotBlank
     String title,
     String description,
     Status status,
