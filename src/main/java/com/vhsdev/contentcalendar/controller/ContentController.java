9341fffd78fd6077c27888c61811064042250a44
Josh Habdas <habdasj@objectcomputing.com>
2024-06-12 15:10:33 -0400

    add response status for new entries

--- src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/ContentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -34,6 +35,7 @@ public class ContentController {
     );
   }
 
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping("")
   public void create(@RequestBody Content content) {
     repository.save(content);
