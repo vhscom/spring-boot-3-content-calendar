0676f3d66873e59da698a445aed959325d913271
Josh Habdas <habdasj@objectcomputing.com>
2024-06-13 14:35:01 -0400

    externalize welcome message to config

--- src/main/java/com/vhsdev/contentcalendar/controller/HomeController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.vhsdev.contentcalendar.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+  @Value("${cc.welcomeMessage:Welcome to Content Calendar!}")
+  private String welcomeMessage;
+
   @GetMapping("/")
   public String home() {
-    return "Welcome to Content Calendar!";
+    return welcomeMessage;
   }
 }
