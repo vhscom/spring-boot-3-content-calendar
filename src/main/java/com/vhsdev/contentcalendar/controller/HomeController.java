0190104d7f4ac54117f3f7b96d00d0d74a55f413
Josh Habdas <habdasj@objectcomputing.com>
2024-06-13 14:44:58 -0400

    use multiple config items in a map

--- src/main/java/com/vhsdev/contentcalendar/controller/HomeController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.vhsdev.contentcalendar.controller;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,11 @@ public class HomeController {
   @Value("${cc.welcomeMessage:Welcome to Content Calendar!}")
   private String welcomeMessage;
 
+  @Value("${cc.about:This is a simple content calendar application.}")
+  private String about;
+
   @GetMapping("/")
-  public String home() {
-    return welcomeMessage;
+  public Map<String, String> home() {
+    return Map.of("welcomeMessage", welcomeMessage, "about", about);
   }
 }
