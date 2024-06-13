59790d597a8c44ab7f7a6742d033ed6919e84c67
Josh Habdas <habdasj@objectcomputing.com>
2024-06-13 14:46:35 -0400

    clean-up existing home controller config

--- src/main/java/com/vhsdev/contentcalendar/controller/HomeController.java
+++ src/main/java/com/vhsdev/contentcalendar/controller/HomeController.java
@@ -1,21 +1,13 @@
 package com.vhsdev.contentcalendar.controller;
 
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-  @Value("${cc.welcomeMessage:Welcome to Content Calendar!}")
-  private String welcomeMessage;
-
-  @Value("${cc.about:This is a simple content calendar application.}")
-  private String about;
-
   @GetMapping("/")
-  public Map<String, String> home() {
-    return Map.of("welcomeMessage", welcomeMessage, "about", about);
+  public String home() {
+    return null;
   }
 }
