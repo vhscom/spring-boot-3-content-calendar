7257aac206b54c0121747b2408bc2fd180d2afce
Josh Habdas <habdasj@objectcomputing.com>
2024-06-13 13:19:34 -0400

    use id annotation from spring data

--- src/main/java/com/vhsdev/contentcalendar/model/Content.java
+++ src/main/java/com/vhsdev/contentcalendar/model/Content.java
@@ -2,8 +2,10 @@ package com.vhsdev.contentcalendar.model;
 
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
 
 public record Content(
+    @Id
     Integer id,
     // If we did nothing else, this wouldn't work. Use along with @Valid in the controller.
     @NotBlank
