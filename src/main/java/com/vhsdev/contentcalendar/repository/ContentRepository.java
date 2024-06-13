3f4b5c9c82d0a86d41d1743af05d25e841fe74f0
Josh Habdas <habdasj@objectcomputing.com>
2024-06-13 13:55:40 -0400

    implement abstract spring data query method

--- src/main/java/com/vhsdev/contentcalendar/repository/ContentRepository.java
+++ src/main/java/com/vhsdev/contentcalendar/repository/ContentRepository.java
@@ -1,8 +1,14 @@
 package com.vhsdev.contentcalendar.repository;
 
 import com.vhsdev.contentcalendar.model.Content;
+import java.util.List;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface ContentRepository extends ListCrudRepository<Content, Integer> {
 
+  /**
+   * Abstract method to find all content by title containing a keyword.
+   * This will be generated as a query method by Spring Data.
+   */
+  List<Content> findAllByTitleContains(String keyword);
 }
